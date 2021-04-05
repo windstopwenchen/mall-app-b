0b0785af2425c8b5428e6d144dc48be39e237999
windstopwenchen <810660250@qq.com>
2021-04-05 18:02:53 +0800

    second-layout

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 切换菜单的闭合状态 false为不闭合
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
